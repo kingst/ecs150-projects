cb85b91f07874203fef59e15ffeeb4ba1a848bd2
Sam King <kingst@gmail.com>
2024-05-07 15:53:57 -0700

    Add stubs for LocalFileSystem.cpp

--- project4-wip/gunrock_web/LocalFileSystem.cpp
+++ project4-wip/gunrock_web/LocalFileSystem.cpp
@@ -1,6 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <assert.h>
+
 #include "LocalFileSystem.h"
-#include "Disk.h"
+#include "ufs.h"
+
+using namespace std;
+
 
 LocalFileSystem::LocalFileSystem(Disk *disk) {
   this->disk = disk;
+}
+
+void LocalFileSystem::readSuperBlock(super_t *super) {
+
+}
+
+int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+  return 0;
+}
+
+int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+  return 0;
+}
+
+int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+  return 0;
+}
+
+int LocalFileSystem::create(int parentInodeNumber, int type, string name) {
+  return 0;
+}
+
+int LocalFileSystem::write(int inodeNumber, const void *buffer, int size) {
+  return 0;
+}
+
+int LocalFileSystem::unlink(int parentInodeNumber, string name) {
+  return 0;
 }
